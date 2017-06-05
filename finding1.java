027cd47cf4b05df5ac3c20f81bda3bd3a31a10a9
dharani-ksr <dharani020896@gmail.com>
2017-06-05 15:55:15 +0530

    Create finding1.java

---
+++ finding1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class finding1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+if(a==0)
+{
+System.out.println("zero");
+}
+}
+}
