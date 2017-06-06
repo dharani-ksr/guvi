9af0bea8bbca39f3ff866395fe42e6a9597ebf1c
dharani-ksr <dharani020896@gmail.com>
2017-06-06 16:17:59 +0530

    Create hel.java

---
+++ hel.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class hel
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.nextLine();
+for(int i=0;i<5;i++)
+{
+System.out.println("hello");
+}
+}
+}
