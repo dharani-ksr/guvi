5c4ff6858bfa44edf72a99ff103a58a9d043d1f8
dharani-ksr <dharani020896@gmail.com>
2017-06-06 15:49:40 +0530

    Create alp.java

---
+++ alp.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class alp
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next().charAt(0);
+if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("not alphabet");
+}
+}
+}
+
