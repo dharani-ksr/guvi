99073696b6d5e8a4436259379fbce29902703cfb
dharani-ksr <dharani020896@gmail.com>
2017-06-08 05:37:47 -0700

    Create ind1.java

---
+++ ind1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class ind1
+{
+public static void main(String args[])
+{
+int i;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b[]=new int[a];
+for(i=0;i<a;i++)
+{
+b[i]=sc.nextInt();
+}			
+for(i=0;i<a;i++)
+{
+if(b[i]==i)
+{
+System.out.println(b[i]);
+}
+}
+}
+}
